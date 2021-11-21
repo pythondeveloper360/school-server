ee0960345a897e73010015667e071e3de15d5c15
Hanzala Khan <pytohndeveloper360@gmail.com>
2021-11-21 05:12:22 +0500

    csd

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import sql
 
 app = FastAPI()
 
-origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
