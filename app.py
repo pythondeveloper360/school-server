df7b20d13bd28adec33cd61fedac15eaedec0051
Hanzala Khan <pytohndeveloper360@gmail.com>
2021-12-17 22:46:35 +0500

    dfs

--- app.py
+++ app.py
@@ -29,18 +29,21 @@ async def getWork(request: Request):
             return{"work": sql.getAllWork(credentials['class'], credentials['section'])}
     else:
         return {'status': 'error'}
+
+
 @app.get('/parentWorks')
-async def getParentWorks(req:Request):
+async def getParentWorks(req: Request):
     if req.headers.get('phone'):
         if sql.checkParent(req.headers.get('phone')):
-            return sql.getAllWorkForParent(phone = req.headers.get('phone'))
+            return {"stutus": True, "works": sql.getAllWorkForParent(phone=req.headers.get('phone'))}
         else:
-            return {"status":'eruturor'}
-    else:return {"status":'error'}
+            return {"status": 'eruturor'}
+    else:
+        return {"status": 'error'}
 
 
 @app.get('/workById')
-async def workById(req:Request):
+async def workById(req: Request):
     if req.headers.get('id'):
         work = sql.getWorkWithId(req.headers.get('id'))
     return work if work else {'status': 'error'}
@@ -54,38 +57,42 @@ async def auth(request: Request):
 
 
 @app.get('/authTeacher')
-async def authTeacher(req:Request):
-    email,password = req.headers.get('email'),req.headers.get("password")
-    work = sql.authTeacher(email = email,password= password)
-    return {'auth':True} if work else {'auth':False}
+async def authTeacher(req: Request):
+    email, password = req.headers.get('email'), req.headers.get("password")
+    work = sql.authTeacher(email=email, password=password)
+    return {'auth': True} if work else {'auth': False}
+
 
 @app.get('/onDayWork')
-async def onDayWork(req:Request):
+async def onDayWork(req: Request):
     work = False
     if req.headers.get('gr'):
         cre = sql.getCredential(req.headers.get('gr'))
-        work = sql.checkOnDayWork(_class = cre.get('class'),section= cre.get('section')) if cre else False
+        work = sql.checkOnDayWork(_class=cre.get(
+            'class'), section=cre.get('section')) if cre else False
     elif req.headers.get('email'):
         cre = sql.getTeacherCredential(req.headers.get('email'))
-        work = sql.checkOnDayWork(_class = cre.get('class'),section= cre.get('section')) if cre else False
-    return {'status':True} if work else {"status":False}
+        work = sql.checkOnDayWork(_class=cre.get(
+            'class'), section=cre.get('section')) if cre else False
+    return {'status': True} if work else {"status": False}
+
 
 @app.post('/uploadWork')
-async def uploadWork(req:Request):
+async def uploadWork(req: Request):
     jData = await req.json()
     if jData.get('email') and jData.get('cw') and jData.get('password'):
-        if sql.authTeacher(jData.get('email'),jData.get('password')):
+        if sql.authTeacher(jData.get('email'), jData.get('password')):
             teacher = sql.getTeacherCredential(jData.get('email'))
             if teacher:
-                work = sql.insertWork(section = teacher.get('section'),_class = teacher.get('class'),hw= jData.get('hw'),cw  = jData.get('cw'))
+                work = sql.insertWork(section=teacher.get('section'), _class=teacher.get(
+                    'class'), hw=jData.get('hw'), cw=jData.get('cw'))
                 if work:
                     return work
                 else:
-                    return{"status":False}
+                    return{"status": False}
             else:
-                return{"status":False}
-        else:{"stauts":'unauthorized'}
+                return{"status": False}
+        else:
+            {"stauts": 'unauthorized'}
     else:
-        return{"status":False}
-    
-
+        return{"status": False}
