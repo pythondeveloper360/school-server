fe94da7e46b17dad51cd160a27604940abc4d3ef
Hanzala Khan <pytohndeveloper360@gmail.com>
2022-03-06 20:54:38 +0500

    gxz

--- app.py
+++ app.py
@@ -128,7 +128,6 @@ async def authStudent(req: Request):
 @app.get('/onDayWork')
 async def onDayWork(req: Request):
     work = False
-    print(req.headers)
     if req.headers.get('gr'):
         cre = sql.getCredential(req.headers.get('gr'))
         work = sql.checkOnDayWork(_class=cre.get(
@@ -137,8 +136,7 @@ async def onDayWork(req: Request):
         cre = sql.getTeacherCredential(req.headers.get('email'))
         work = sql.checkOnDayWork(_class=cre.get(
             'class'), section=cre.get('section'), date=req.headers.get('date')) if cre else False
-        print(cre)
-        print(work)
+
     return {'status': True} if work else {"status": False}
 
 
