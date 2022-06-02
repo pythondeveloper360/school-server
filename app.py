fa383d87006573467657fec2400cdd73d4a2700f
Hanzala Khan <pythondeveloper360@gmail.com>
2022-06-03 02:13:29 +0500

    cx

--- app.py
+++ app.py
@@ -38,8 +38,7 @@ async def getWork(request: Request):
 async def seenByStudents(req: Request):
     if req.headers.get('id'):
         return {'students': sql.seenByStudents(req.headers.get('id')), "status": True}
-    else:
-        return {"status": False}
+    return {"status": False}
 
 
 @app.get('/workById')
@@ -66,7 +65,7 @@ async def auth(request: Request):
     elif email and password and sql.authTeacher(email=email, password=password):
         credentials = sql.getTeacherCredential(email=email)
         return {'auth': True, "works": sql.getAllWork(_class=credentials.get('class'), section=credentials.get('section'))}
-    elif phone and sql.checkParent(phone=phone):
+    elif phone and sql.authParent(phone=phone):
         return {'auth': True, "works": sql.getAllWorkForParent(phone=phone)}
     return {'auth': False}
 
@@ -79,54 +78,17 @@ async def onDayWork(req: Request):
     return {'todayWork': True} if work else {"todayWork": False}
 
 
-# @app.get('/getTPmessages')
-# async def getTPmessages(req: Request):
-#     conditions = [req.headers.get('teacherEmail'), req.headers.get(
-#         'studentGR'), req.headers.get('parentPhone')]
-#     if conditions[1] and any(*conditions[1:]):
-#         work = sql.getTPMessages(
-#             parentPhone=conditions[2], teacherEmail=conditions[0], studentGr=conditions[1])
-#         return {"status":True,"messages":work} if work else {"status":False}
-#     else:
-#         return False
-
-# @app.get('/getTPChats')
-# async def getTPChats(req: Request):
-#     conditions = [req.headers.get('teacherEmail'), req.headers.get(
-#         'parentPhone'), req.headers.get('studentGr')]
-#     if any(conditions):
-#         work = sql.getTPChats(
-#             teacherEmail=conditions[0], parentPhone=conditions[1], studentGr=conditions[2])
-#         return {'chats': work, "status": True} if work else {"status": False}
-#     else:
-#         return {"status": False}
-
-@app.websocket('/ws')
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_json()
-        if (data):
-            ...
-            # print(data)
-
-
 @app.post('/uploadWork')
 async def uploadWork(req: Request):
     jData = await req.json()
-    if jData.get('email') and jData.get('work') and jData.get('password'):
-        if sql.authTeacher(jData.get('email'), jData.get('password')):
-            teacher = sql.getTeacherCredential(jData.get('email'))
-            if teacher:
-                work = sql.insertWork(section=teacher.get('section'), _class=teacher.get(
-                    'class'), work=jData.get('work'), date=jData.get('date'))
-                if work:
-                    return {'status': True, "data": work}
-                else:
-                    return{"status": False}
-            else:
-                return{"status": False}
-        else:
-            {"stauts": 'unauthorized'}
+    if 'email' in jData and 'work' in jData and 'password' in jData:
+        auth = sql.authTeacher(jData.get('email'), jData.get('password'))
+        teacher = sql.getTeacherCredential(jData.get('email'))
+        if auth and teacher:
+            work = sql.insertWork(section=teacher.get('section'), _class=teacher.get(
+                'class'), work=jData.get('work'), date=jData.get('date'))
+            if work:
+                return {'status': True, "data": work}
+
     else:
         return{"status": False}
--- sql.py
+++ sql.py
@@ -164,35 +164,12 @@ def getTeacherCredential(email: str):
     return {"email": data[0], 'name': data[1], "class": data[2], "section": data[3]} if data else False
 
 
-def newTeacher(email: str, section: str, _class: str, name: str):
-    sqlquery = sql.SQL('insert into teachers ({email},{password},{name},{section},{_class}) values (%s,%s,%s,%s,%s)').format(
-        email=sql.Identifier("email"),
-        password=sql.Identifier("password"),
-        name=sql.Identifier("name"),
-        section=sql.Identifier("section"),
-        _class=sql.Identifier("class")
-    )
-    cursor.execute(sqlquery, (email, email, name, section, _class))
-    db.commit()
-
-
-def checkParent(phone):
+def authParent(phone):
     sqlquery = sql.SQL(
         'select {phone} from parents where {phone}  = %s').format(phone=sql.Identifier("phone"))
     cursor.execute(sqlquery, (phone,))
     data = cursor.fetchall()
     return True if data else False
-
-
-def createParent(phone, children: list):
-    if not checkParent(phone=phone):
-        sqlquery = sql.SQL('insert into parents ({id},{phone},{children}) values (%s,%s,%s)').format(
-            id=sql.Identifier("id"), phone=sql.Identifier("phone"), children=sql.Identifier("children"))
-        cursor.execute(sqlquery, (idGenerator(), phone, children))
-        db.commit()
-        return True
-    else:
-        return False
 
 
 def getAllWorkForParent(phone):
@@ -239,69 +216,3 @@ def seenWork(id, by):
         return False
     # data.append(by)
 
-
-def AuthStaff(username, password):
-    if username == '' or password == '':
-        return False
-    sqlquery = sql.SQL('select name from staff where {username} = %s and {password} = %s').format(
-        username=sql.Identifier("username"), password=sql.Identifier("password"))
-    cursor.execute(sqlquery, (username, password))
-    data = cursor.fetchone()
-    data = data[0] if data else False
-    return data
-
-
-def allTeachers():
-    sqlquery = sql.SQL('select {email}, {name},{_class},{section} from teachers').format(
-        email=sql.Identifier("email"),
-        name=sql.Identifier("name"),
-        _class=sql.Identifier("class"),
-        section=sql.Identifier("section")
-    )
-    cursor.execute(sqlquery)
-    data = cursor.fetchall()
-    return [{'email': i[0], 'name':i[1], 'class':i[2], "section":i[3]} for i in data]
-# ! need to uncomment after sometime
-
-# def getTPChats(parentPhone='', teacherEmail='', studentGr=''):
-#     if all([parentPhone, checkParent(parentPhone)]):
-#         sqlquery = sql.SQL('select {techerName},{teacherEmail} from TPmessages where {parent} = %s').format(
-#             teacherEmail=sql.Identifier("teacher"), parent=sql.Identifier("parent"), teacherName=sql.Identifier("name"))
-#         cursor.execute(sqlquery, (parentPhone,))
-#         data = cursor.fetchall()
-#         return list(set([{'name': i[0], "email":i[1]} for i in data])) if data else []
-#     elif teacherEmail:
-#         sqlquery = sql.SQL('select {parentname},{msg},{studentname},{parent},{student},{by},{msgTo},{forstudent} from TPmessages where {teacher} = %s').format(
-#             parent=sql.Identifier("parent"), student=sql.Identifier("student"), teacher=sql.Identifier("teacher"), msg=sql.Identifier("message"),
-#             studentname=sql.Identifier("studentname"), parentname=sql.Identifier("parentname"), msgTo=sql.Identifier("msgto"), by=sql.Identifier("by"))
-#         cursor.execute(sqlquery, (teacherEmail,))
-#         data = cursor.fetchall()
-#         rl = []
-#         for i in data:
-#             if i[5] in ['teacher', 'parent'] or i[6] in ['teacher', 'parent']:
-#                 rl.append(
-#                     {'parentName': i[0], 'parentPhone': i[3], "msg": i[1]})
-#             elif i[5] in ['student', 'teacher'] or i[6] in ['teacher', 'student']:
-#                 rl.append({'studentName': i[2], "studentGr": [4]})
-#         # return list(set(rl))
-#         return rl
-#     elif studentGr:
-#         sqlquery = sql.SQL('select {teacher} from TPmessages where {student} = %s').format(
-#             teacher=sql.Identifier("teacher"), student=sql.Identifier("student"))
-#         cursor.execute(sqlquery, (studentGr,))
-#         data = cursor.fetchall()
-#         return list(set([i[0] for i in data])) if data else []
-#     else:
-#         return False
-
-
-# def sendTPmessage(teaherEmail: str, msg: str, by: str, date: str, forStudent: str, studentGr: str = '', parentPhone: str = ''):
-#     if all([checkParent(phone=parentPhone), by in ['teacher', 'parent'], forStudent]):
-#         sqlquery = sql.SQL('insert into TPmessages({id},{message},{parent},{teacher},{by},{date},{forStudent}) values(%s,%s,%s,%s,%s,%s,%s)').format(id=sql.Identifier(
-#             "id"), message=sql.Identifier("message"), parent=sql.Identifier("parent"), teacher=sql.Identifier("teacher"), by=sql.Identifier("by"), forStudent=sql.Identifier("forstudent"))
-#         cursor.execute(sqlquery, (idGenerator(), msg,
-#                        parentPhone, teaherEmail, by, date, forStudent))
-#         db.commit()
-#         return True
-#     else:
-#         return False
